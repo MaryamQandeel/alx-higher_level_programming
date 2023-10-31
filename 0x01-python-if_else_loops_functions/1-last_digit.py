c2e0fec3f7709242bd4cb3f38df9ae146747a8b7
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-01 01:15:18 +0300

    last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if ld > 5:
 elif ld == 0:
     print("{}{:d} is {:d} and is 0".format(line, number, ld))
 else:
-    less = "and is less than 6 and not"
+    less = "and is less than 6 and not 0"
     print("{}{:d} is {:d} {}".format(line, number, ld, less))
