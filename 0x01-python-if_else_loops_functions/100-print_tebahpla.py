7fc2a1c6ae796b8b78667511e1a2423428aa1e12
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-01 02:42:23 +0300

    print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
     ch = i + ord("A")
-    if i %2 == 1:
+    if i % 2 == 1:
         ch = ch + 32
-    print ("{:c}".format(i), end="")
+    print("{:c}".format(i), end="")
