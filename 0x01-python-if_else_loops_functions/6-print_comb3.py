d5c0ada6181744a1c3df20becac39b6820dc9ca9
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-01 03:06:53 +0300

    print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for n in range(i, 10):
         if i > n:
-            if i ==8 and n ==9:
+            if i == 8 and n == 9:
                 print("{:d}{:d}".format(i, n), end="\n")
             else:
                 print("{:d}{:d}".format(i, n), end=", ")
