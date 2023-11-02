fc85bef69a65a336c4db6203f9f3957c209c1c82
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-03 00:46:10 +0200

    infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+  total = int
+    for i in range(len(sys.argv) - 1):
+        total = total + int sys.argv[i + 1])
+        print(total)
+        
