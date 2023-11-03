559163dfd051889091998a814a6cd987125447ba
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-03 02:10:43 +0200

    my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    imoirt sys
+    import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
