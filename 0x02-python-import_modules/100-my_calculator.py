f4f4d46e27ba319ddddae82b9a3b41119029e521
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-03 02:07:28 +0200

    my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int len(sys.argv[1])
-    b = int len(sys.argv[2])
+    a = int (sys.argv[1])
+    b = int (sys.argv[2])
     operator = sys.argv[2]
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
