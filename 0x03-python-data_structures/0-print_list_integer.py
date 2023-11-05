3d5cb780b3c5b7c49be67488e6c17f14b266f00c
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-06 01:37:27 +0200

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     len = my_list.length
-    for i in range(my_list[0], my_list[len+1])
+    for i in range(my_list[0], my_list[len+1]):
         print("{:d}\n".format(i)
