f74ad8360808074a41dfb0be32c5ba52a3047844
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-06 03:08:56 +0200

    Create 4-new_in_list.p

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx >= len(my_list) or idx < 0:
+        return my_list
+    else:
+        return my_list
+        my_list[idx] = element
+        return my_list
