15492a1139d07644b18ca160e88bc45b10ab3e8c
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-07 01:02:47 +0200

    Create 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    for i in my_string:
+        if i != "c" or i != "C":
+            return my_string
+        else:
+            if i == "c" or i == "C":
+                new_string[i] == ""
+            return new_string
