2e5a1ebcdf4591dbb1110363c1a7cc6b142821e6
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-11-09 01:23:40 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,8 @@
-f
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for i in matrix:
+        for j in range(len(i)):
+            new_num = j*j
+           new_matrix = print("{:d}".format(new_num), end="")
+             
+       
