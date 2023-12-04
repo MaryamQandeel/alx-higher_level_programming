3a620a311c5806f0362a894bc969c2ba0ca7f654
MaryamQandeel <123130072+MaryamQandeel@users.noreply.github.com>
2023-12-05 01:36:15 +0200

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
-
+"""initializate"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
+    """Square1"""
+
     def __init__(self, size):
-        """ instantiation with size """
+        """initial attributes"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+        self.area()
